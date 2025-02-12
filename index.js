437e882ad3671eebfe5a04b4e441f70bcc0a7dcb
git <git@git.com>
2025-02-12 15:31:38 +0800

    194保養時數Change

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ async function handleEvent(event) {
       }
 
       const [equipmentRows] = await db.query(
-        "SELECT 設備編號, 設備狀態, 運轉時數, 日期, 使用地點, 上次保養時間, 上次運轉時數 FROM 設備資料表 WHERE 設備編號 = ?",
+        "SELECT 設備編號, 設備狀態, 運轉時數, 日期, 使用地點, 上次保養時間, 上次保養時數 FROM 設備資料表 WHERE 設備編號 = ?",
         [deviceId]
       );
       // 確保變數有值，避免 ReferenceError
